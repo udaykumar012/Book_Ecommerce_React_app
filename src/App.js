a5d19ecf57227221ac9cca58ce2249bda81c3501
udaykumar012 <uday_kumar_kotla@epam.com>
2022-06-26 19:54:43 +0530

    small route change on app.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Route path='/checkout' element={<Checkout/>}/>
       <Route path='/checkout/:title'  element={<Checkout/>} />
       <Route path='/cart'  element={<Cart/>} />
-      <Route path="/" element={<Navigate replace to="/home" />} />
+      <Route path="/" element={<Home/>} />
       <Route path='/orderaccepted' element={<OrderAccepted/>}/>
       <Route path='/myOrders' element={<MyOrders/>}/>
     </Routes>
